9e8cd82a89a58cf45c2412d41a22ef4fbaf40fad
AdrianBustanil <rianbustanil450@gmail.com>
2025-06-12 18:57:25 +0700

    catch

--- CircularQueus.cpp
+++ CircularQueus.cpp
@@ -135,7 +135,35 @@ int main()
                     q.insert();
                     break;
                 }
+                case '2':
+                {
+                    q.remove();
+                    break;
+                }
+                case '3':
+                {
+                    q.display();
+                    break;
+                }
+                case '4':
+            {
+                return 0;
             }
+            default:
+            {
+                cout << "Invalid option||" << endl;
+                break;
+            }
+                
+                
+            }
+
+        }
+        catch(exception &e)
+        {
+            cout << "Check for the value entered." << endl;
         }
     }
+
+    return 0,
 }
