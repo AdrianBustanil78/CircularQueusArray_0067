937952490d1ccb2a9549294d936d29be302f029e
AdrianBustanil <rianbustanil450@gmail.com>
2025-06-12 18:50:01 +0700

    try

--- CircularQueus.cpp
+++ CircularQueus.cpp
@@ -71,7 +71,7 @@ public:
     }
     void display()
     {
-        int FRONT_pasitrion = FRONT;
+        int FRONT_pasition = FRONT;
         int REAR_position = REAR;
         
         if (FRONT == -1)
@@ -96,7 +96,7 @@ public:
             while (FRONT_position <= max - 1)
             {
                 cout << queue_array[FRONT_position] << " ";
-                FRONT_pasitrion++;
+                FRONT_pasition++;
             }
             
             FRONT_position = 0;
@@ -109,3 +109,24 @@ public:
         }
     }  
 };
+
+int main()
+{
+    Queues q;
+    char ch;
+
+    while (true)
+    {
+        try
+        {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display values" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
+        }
+    }
+}
