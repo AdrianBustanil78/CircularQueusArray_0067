bd7a031813f3d878b5045f409cf4ad5faa072529
AdrianBustanil <rianbustanil450@gmail.com>
2025-06-12 17:50:13 +0700

    cek apakah antrian penuh

--- CircularQueus.cpp
+++ CircularQueus.cpp
@@ -22,4 +22,18 @@ public:
         cin >> num;
         cout << endl;
       }
+
+      // 1. cek apakah antrian penuh
+      if ((FRONT == 0 && REAR == max -1) || (FRONT == REAR +1))
+      {
+        cout << "\nQueue overflow\n"; // 1.a
+        return;                       // 1.b
+      }
+
+      // 2.cek apakah 
+      if (FRONT == - 1)
+      {
+        FRONT = 0;
+        REAR = 0;
+      } 
 };
